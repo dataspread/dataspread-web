46a59a8111bdb22bffc2c309f2b934ec87742f63
Tana Wattanawaroon <tana.wattanawaroon@gmail.com>
2017-08-23 06:30:08 -0500

    prepared statements

--- zpoi/src/org/zkoss/poi/ss/formula/functions/SqlFunction.java
+++ zpoi/src/org/zkoss/poi/ss/formula/functions/SqlFunction.java
@@ -5,53 +5,80 @@ import org.model.DBHandler;
 import org.zkoss.poi.ss.formula.eval.*;
 
 import java.sql.*;
+import java.util.ArrayList;
 
-public class SqlFunction extends Fixed1ArgFunction {
+public class SqlFunction implements Function {
 
     @Override
-    public ValueEval evaluate(int srcRowIndex, int srcColumnIndex, ValueEval arg0) {
-        ValueEval evaluatedQueryArg = evaluateQueryArg(arg0, srcRowIndex, srcColumnIndex);
+    public ValueEval evaluate(ValueEval[] args, int srcRowIndex, int srcColumnIndex) {
+        int nArgs = args.length;
+        if (nArgs < 1) {
+            // too few arguments
+            return ErrorEval.VALUE_INVALID;
+        }
 
-        if (evaluatedQueryArg instanceof StringEval) {
+        if (nArgs > 30) {
+            // too many arguments
+            return ErrorEval.VALUE_INVALID;
+        }
+        
+        ValueEval evaluatedQuery = evaluateQueryArg(args[0], srcRowIndex, srcColumnIndex);
+        ValueEval[] evaluatedParameters = evaluateParameterArgs(args, srcRowIndex, srcColumnIndex);
+        
+        
+        if (evaluatedQuery instanceof StringEval) {
             try {
-                String q = ((StringEval) evaluatedQueryArg).getStringValue();
-                return new StringEval(query(q));
+                String queryString = ((StringEval) evaluatedQuery).getStringValue();
+                return makeQuery(queryString, evaluatedParameters);
             } catch (SQLException e) {
                 e.printStackTrace();
+                return ErrorEval.VALUE_INVALID;
             }
-
-        }
-        if (evaluatedQueryArg == BlankEval.instance) {
-            return null;
         }
         throw new RuntimeException("Unexpected type for query ("
-                + evaluatedQueryArg.getClass().getName() + ")");
+                + evaluatedQuery.getClass().getName() + ")");
     }
 
-    private String hackCompose(ResultSet rs) throws SQLException {
+    private ArrayEval parseResult(ResultSet rs) throws SQLException {
         ResultSetMetaData metaData = rs.getMetaData();
         int numCols = metaData.getColumnCount();
-        StringBuilder builder = new StringBuilder();
+        ArrayList<StringEval[]> rows = new ArrayList<>();
+        
         while (rs.next()) {
-            for (int i = 1; i <= numCols; i++) {
-                builder.append(rs.getString(i));
-                builder.append(",");
+            StringEval[] row = new StringEval[numCols];
+            for (int i = 0; i < numCols; i++) {
+                row[i] = new StringEval(rs.getString(i+1));
             }
-            builder.append(";");
+            rows.add(row);
         }
-        return builder.toString();
+
+        int numRows = rows.size();
+        
+        StringEval[][] evalSourceArray = new StringEval[rows.size()][];
+        for (int i = 0; i < numRows; i++) {
+            evalSourceArray[i] = rows.get(i);
+        }
+
+        ArrayEval eval = new ArrayEval(evalSourceArray, 0, 0, numRows-1, numCols-1, null);
+        return eval;
     }
 
-    private String query(String sql) throws SQLException {
-
+    private ValueEval makeQuery(String queryString, ValueEval[] parameters) throws SQLException {
         Connection connection = DBHandler.instance.getConnection();
-        Statement stmt = connection.createStatement();
-
-        ResultSet queryResult = stmt.executeQuery(sql);
-        String resultString = hackCompose(queryResult);
+        
+        PreparedStatement stmt = connection.prepareStatement(queryString);
+        for (int i = 0; i < parameters.length; i++) {
+            if (parameters[i] instanceof StringEval) {
+                String paramString = ((StringEval) parameters[i]).getStringValue();
+                stmt.setString(i+1, paramString);
+            }
+        }
+        
+        ResultSet result = stmt.executeQuery();
+        ArrayEval resultEval = parseResult(result);
         connection.commit();
         connection.close();
-        return resultString;
+        return resultEval;
     }
 
     /**
@@ -66,4 +93,21 @@ public class SqlFunction extends Fixed1ArgFunction {
         }
     }
 
+    /**
+     *
+     * @return the de-referenced query arg (possibly {@link ErrorEval})
+     */
+    private static ValueEval[] evaluateParameterArgs(ValueEval[] args, int srcRowIndex, int srcColumnIndex) {
+        ValueEval[] evaluatedArgs = new ValueEval[args.length-1];
+        for (int i = 1; i < args.length; i++) {
+            try {
+                evaluatedArgs[i-1] = OperandResolver.getSingleValue(args[i], srcRowIndex, (short)srcColumnIndex);
+            } catch (EvaluationException e) {
+                evaluatedArgs[i-1] = e.getErrorEval();
+            }
+        }
+        return evaluatedArgs;
+    }
+
+    
 }
