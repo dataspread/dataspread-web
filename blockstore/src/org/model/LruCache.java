165a86ed6ed885381423be63131f1bcb5efa0b05
Mangesh Bendre <mangeshbendre@gmail.com>
2017-09-29 10:47:32 -0500

    Using Google Cache.

--- blockstore/src/org/model/LruCache.java
+++ blockstore/src/org/model/LruCache.java
@@ -1,59 +1,48 @@
 package org.model;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 
 public class LruCache<K, V> {
-    private ConcurrentLinkedQueue<K> concurrentLinkedQueue;
-    private ConcurrentHashMap<K,V> concurrentHashMap;
-    
-    private final int maxEntries;
-    private final int evictSize;
+    Cache<K, V> cache;
 
     // Get the size = maxEntrs, when size >  maxEntries + evictSize
     public LruCache(int maxEntries, int evictSize) {
-        concurrentHashMap = new ConcurrentHashMap<>();
-        concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
-        this.maxEntries = maxEntries;
-        this.evictSize = evictSize;
+        cache = CacheBuilder.newBuilder()
+                .maximumSize(maxEntries)
+                .build();
     }
 
     public boolean containsKey(K key) {
-        return concurrentHashMap.containsKey(key);
+        return cache.asMap().containsKey(key);
     }
 
     public void put(K key, V value) {
-        // Do not store null values.
         if (value==null)
             return;
-        V putVal = concurrentHashMap.put(key,value);
-        if (putVal==null)
-            concurrentLinkedQueue.add(key);
-
-        if (maxEntries > -1 && concurrentLinkedQueue.size() > maxEntries+evictSize) {
-            int itemsToRemove = concurrentLinkedQueue.size() - maxEntries;
-            for (int i = 0; i < itemsToRemove; i++)
-                concurrentHashMap.remove(concurrentLinkedQueue.remove());
-        }
+        cache.put(key, value);
+        // Do not store null values.
     }
 
     public V get(K key) {
-        return concurrentHashMap.get(key);
+        return cache.getIfPresent(key);
     }
 
     public void remove(K key) {
-        // Do not remove from queue concurrentLinkedQueue, as it is expensive.
-        concurrentHashMap.remove(key);
+        cache.invalidate(key);
     }
 
     public void clear() {
-        concurrentLinkedQueue.clear();
-        concurrentHashMap.clear();
+        cache.invalidateAll();
     }
 
-    public List<K> keySet() {
-        return concurrentHashMap.keySet().stream().collect(Collectors.toList());
+    public Collection<K> keySet() {
+        return cache.asMap().keySet();
     }
 }
