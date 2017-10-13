02df34ebc1f1b64074c0ca5108ae290dc34f5d57
Mangesh Bendre <mangeshbendre@gmail.com>
2017-10-13 10:49:20 -0500

    Btree Test Code.

--- TestCode/src/BTreeTest.java
+++ TestCode/src/BTreeTest.java
@@ -2,7 +2,10 @@ import org.model.DBContext;
 import org.model.DBHandler;
 import org.zkoss.zss.model.impl.BTree;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class BTreeTest {
@@ -15,14 +18,66 @@ public class BTreeTest {
         DBHandler.connectToDB(url, driver, userName, password);
         DBContext dbContext = new DBContext(DBHandler.instance.getConnection());
         BTree btree = new BTree(dbContext, "Test1");
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
-        IntStream.range(0,10).forEach(e->btree.addByCount(dbContext, e, e*10));
+        // Random operations
+        final int operations=1000;
+        Random random = new Random(1);
+        Random randomOperation = new Random(1);
+        int stats_add =0;
+        int stats_remove =0;
 
-        Arrays.stream(btree.getIDsByCount(dbContext, 1, 5))
-                .forEach(e->System.out.println(e));
+        // Add initial data points.
+        for (int i=0;i<operations;i++)
+        {
+            stats_add++;
+            int randomValue = random.nextInt();
+            List<Integer> insertList = new ArrayList<>();
+            insertList.add(randomValue);
+            btree.insertIDs(dbContext, i, insertList);
+            arrayList.add(i,randomValue);
+        }
+
+        // Perform RANDOM operations.
+        for (int i=0;i<operations;i++)
+        {
+            int operation = randomOperation.nextInt(2);
+            int randomPos = random.nextInt(arrayList.size());
+            int randomValue = random.nextInt();
+
+            switch (operation)
+            {
+                case 0:
+                    stats_add++;
+                    List<Integer> randomValueList = new ArrayList<>();
+                    randomValueList.add(randomValue);
+                    //System.out.println(btree.size(dbContext) + " add " + randomPos + " " + randomValueList);
+                    btree.insertIDs(dbContext, randomPos, randomValueList);
+                    arrayList.add(randomPos, randomValue);
+                    break;
+                case 1:
+                    stats_remove++;
+                    List<Integer> randomPosList = new ArrayList<>();
+                    //System.out.println(btree.size(dbContext) + " remove " + randomPos);
+                    btree.deleteIDs(dbContext, randomPos, 1);
+                    arrayList.remove(randomPos);
+                    break;
+            }
+        }
+
+
+        System.out.println("Records added:" + stats_add);
+        System.out.println("Records deleated:" + stats_remove);
+        System.out.println("BTree size:" + btree.size(dbContext));
+        System.out.println("ArrayList size:" + arrayList.size());
+
+        if (btree.size(dbContext) == arrayList.size()
+                && Arrays.asList((btree.getIDsByCount(dbContext, 0, arrayList.size())))
+                .equals(arrayList))
+            System.out.println("Results Match");
+        else
+            System.err.println("Results do not match");
 
         dbContext.getConnection().close();
-
-
     }
 }
