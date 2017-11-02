bad9bca8f1d3bb4a7b2560bf6b36264e7e8f27a0
rahmansunny071 <rahmansunny071@gmail.com>
2017-11-01 23:42:33 -0500

    importSheet edited to support navigation: DB create, populate, load->create nav structure

--- zssmodel/src/org/zkoss/zss/model/impl/ROM_Model.java
+++ zssmodel/src/org/zkoss/zss/model/impl/ROM_Model.java
@@ -11,6 +11,7 @@ import org.postgresql.jdbc.PgConnection;
 import org.zkoss.zss.model.CellRegion;
 import org.zkoss.zss.model.SSheet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.*;
@@ -22,7 +23,9 @@ public class ROM_Model extends Model {
     private static final Logger logger = Logger.getLogger(ROM_Model.class.getName());
     private PosMapping rowMapping;
     private PosMapping colMapping;
-
+    private List<Bucket<String>> navSbuckets;
+    private ArrayList<String> recordList;
+    private int kHisto = 5;
 
     //Create or load RCV_model.
     ROM_Model(DBContext context, SSheet sheet, String tableName) {
@@ -30,6 +33,8 @@ public class ROM_Model extends Model {
         rowMapping = new BTree(context, tableName + "_row_idx");
         colMapping = new BTree(context, tableName + "_col_idx");
         this.tableName = tableName;
+        this.navSbuckets = new ArrayList<Bucket<String>>();
+        this.recordList = new ArrayList<String>();
         createSchema(context);
     }
 
@@ -365,6 +370,25 @@ public class ROM_Model extends Model {
         colMapping.clearCache(context);
     }
 
+    public void createNavSchema(String firstRow,String indexCol)
+    {
+        StringBuffer createTable = new StringBuffer("CREATE TABLE IF NOT EXISTS ");
+        createTable.append(tableName+" ");
+
+        createTable.append(firstRow);
+
+        StringBuffer indexTable = new StringBuffer("CREATE INDEX col_index ON ");
+        indexTable.append(tableName+" ("+indexCol+")");
+        try (AutoRollbackConnection connection = DBHandler.instance.getConnection();
+        Statement createStmt = connection.createStatement();
+        Statement indexStmt = connection.createStatement()) {
+            createStmt.executeUpdate(createTable.toString());
+            indexStmt.execute(indexStmt.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void importSheet(Reader reader, char delimiter) throws IOException {
         final int COMMIT_SIZE_BYTES = 8 * 1000 * 1000;
@@ -427,10 +451,298 @@ public class ROM_Model extends Model {
 
     }
 
+    public void importNavSheet(Reader reader, char delimiter) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(reader);
+        String line;
+        String headerString = "";
+        String indexString = "";
+        String valuesString = "";
+        int selectedCol = 0;
+        int sampleSize = getSampleSize();
+
+        CSVReader csvReader = new CSVReader(reader, delimiter);
+        String[] nextLine;
+        int importedRows = 0;
+        int importedColumns = 0;
+        int insertedRows = 0;
+        logger.info("Importing sheet");
+
+        //create table schema and index
+        try{
+
+            while((line=br.readLine())!=null)
+            {
+                String [] columns = line.split(String.valueOf(delimiter));
+                importedColumns = columns.length;
+                StringBuffer str = new StringBuffer();
+                StringBuffer header = new StringBuffer("(");
+                StringBuffer values = new StringBuffer("?");
+
+                str.append(columns[0]+" TEXT");
+                header.append(columns[0]);
+                for (int i = 1; i < columns.length; i++) {
+                    str.append(", ")
+                            .append(columns[i] + " TEXT");
+                    header.append(", ")
+                            .append(columns[i]);
+                    values.append(",?");
+                }
+                str.append(")");
+
+                headerString = "("+header.toString()+")";
+                valuesString = values.toString();
+                indexString = columns[selectedCol];
+                createNavSchema(str.toString(),indexString);
+                break;
+            }
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+
+        try (AutoRollbackConnection connection = DBHandler.instance.getConnection()) {
+            DBContext dbContext = new DBContext(connection);
+
+            StringBuffer sb = new StringBuffer();
+            while ((nextLine = csvReader.readNext()) != null)
+            {
+                if(importedRows==0)
+                {
+                    ++importedRows;
+                    continue;
+                }
+
+                sb.append("INSERT into "+tableName+" "+headerString+" values("+valuesString+")");
+
+                PreparedStatement pst = null;
+
+                pst = connection.prepareStatement(sb.toString());
+
+                for(int i=0;i<importedColumns;i++)
+                    pst.setString(i+1,nextLine[i]);
+
+                pst.executeUpdate();
+
+                ++importedRows;
+
+                if ((importedRows-1)% sampleSize ==0 && importedRows!=1) {
+                    connection.commit();
+
+                    createNavS(headerString,indexString,insertedRows==0?true:false);
+
+                    insertRows(dbContext, insertedRows, importedRows-1);
+                    insertedRows += (importedRows-1);
+
+                    logger.info((importedRows-1) + " rows imported ");
+                }
+
+
+            }
+
+            insertRows(dbContext, 0, importedRows);
+            logger.info("Import done: " + importedRows + " rows and "
+                    + importedColumns + " columns imported");
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void createNavS(String headerString,String indexString,boolean isFirst) {
+        //load sorted data from table
+        recordList =  new ArrayList<String>();
+        try (AutoRollbackConnection connection = DBHandler.instance.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet rs = statement.executeQuery("SELECT "+indexString+" FROM " + tableName+" ORDER by "+indexString);
+
+
+            while (rs.next())
+                recordList.add(rs.getString(1));
+
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //create nav data structure
+
+        navSbuckets = getBucketsNoOverlap(1,recordList.size());
+
+
+    }
+
+
+    private int getSampleSize() {
+        return 10;
+    }
+
     @Override
     public boolean deleteTableColumns(DBContext dbContext, CellRegion cellRegion) {
         throw new UnsupportedOperationException();
     }
 
+    public List<Bucket<String>> getBucketsNoOverlap(int startPos, int endPos)
+    {
+        List<Bucket<String>> bucketList = new ArrayList<Bucket<String>>();
+
+        if (recordList.size()>0) {
+
+            int bucketSize = (endPos-startPos+1) / kHisto;
+            int boundary_change = 0;
+            int element_count = 0;
+            int startIndex=startPos;
+            for (int i = 0; i < kHisto && startIndex < endPos; i++) {
+                Bucket bucket = new Bucket();
+                bucket.minValue = recordList.get(startIndex);
+                bucket.startPos = startIndex;
+                if(startIndex+bucketSize-1 < endPos) {
+                    bucket.maxValue = recordList.get(startIndex + bucketSize - 1);
+                    bucket.endPos = startIndex + bucketSize - 1;
+                }
+                else
+                {
+                    bucket.maxValue = recordList.get(endPos-1);
+                    bucket.endPos = endPos - 1;
+
+                }
+
+                /*
+                * if the value next to maxValue is same as maxValue, we need to increase bucket boundary
+                * Search where max value ends: binary search in maxValue index+bucketSize
+                * Search where maxValue stated in current bucket
+                * if count maxValue in current bucket > count maxValue in next bucket. Merge the two else update current
+                * bucket boundary to be the index just before the maxValue in current bucket
+                * */
+                int bounday_inc = 0;//count maxValue in next bucket
+                int bounday_dec = 0;//count maxValue in current bucket
+
+                if(recordList.size()-1-startIndex+1 < bucketSize)
+                    bucketSize = recordList.size()-1-startIndex; //forcefully set bucket 1 size smaller to pass through next if else
+
+                // System.out.println("startIndex: "+startIndex+", bucketSize: "+bucketSize+", subList.size(): "+subList.size());
+
+
+                if((startIndex + bucketSize) < recordList.size()) //if not end of list
+                {
+                    String boundary_value = recordList.get(startIndex + bucketSize);
+
+                    if(boundary_value.equals(bucket.maxValue))
+                    {
+                        bounday_inc++;
+                        //Search where max value ends in next bucket
+                        for(int j=startIndex + bucketSize+1 ; j<endPos;j++)
+                        {
+                            boundary_value = recordList.get(j);
+                            if(boundary_value.equals(bucket.maxValue)) {
+                                bounday_inc++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        //count maxValue in current bucket
+                        for(int j=startIndex + bucketSize-1;j>startIndex -1;j--)
+                        {
+                            boundary_value = recordList.get(j);
+                            if(boundary_value.equals(bucket.maxValue)) {
+                                bounday_dec++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if(bounday_dec>bounday_inc)//keep everything in current bucket
+                        {
+                            bucket.maxValue = recordList.get(startIndex + bucketSize - 1+bounday_inc);
+                            bucket.endPos = startIndex + bucketSize - 1+bounday_inc;
+                            boundary_change = bounday_inc;
+                        }
+                        else
+                        {
+                            bucket.maxValue = recordList.get(startIndex + bucketSize - 1-bounday_dec);
+                            bucket.endPos = startIndex + bucketSize - 1-bounday_dec;
+                            boundary_change = -bounday_dec;
+                        }
+                    }
+
+                }
+
+
+                bucket.size = bucketSize+boundary_change;
+                bucket.childCount = kHisto;
+                if(bucket.size>0) {
+                    startIndex += bucket.size;
+                    bucketList.add(bucket);
+
+                }
+
+
+                if(bounday_dec < bounday_inc) //create new bucket as the current bucket is shrinked
+                {
+                    Bucket bucketSplit = new Bucket();
+                    bucketSplit.minValue = recordList.get(startIndex);
+                    bucketSplit.maxValue = recordList.get(startIndex+bounday_dec+bounday_inc-1);
+                    bucketSplit.size = bounday_dec+bounday_inc;
+                    bucketList.add(bucketSplit);
+
+                    if(bucket.size >0)
+                        i++;
+                    startIndex += bucketSplit.size;
+
+                }
+
+            }
+
+            if(startIndex<endPos)
+            {
+                Bucket bucket = new Bucket();
+                bucket.minValue = recordList.get(startIndex);
+                bucket.maxValue = recordList.get(endPos-1);
+                bucket.size = endPos-1-startIndex+1;
+                bucketList.add(bucket);
+            }
+
+        }
+
+        // printBuckets(bucketList);
+        return bucketList;
+    }
+
+    public void createJSON()
+    {
+
+    }
+
+    static class Bucket<T> {
+        T minValue;
+        T maxValue;
+        int startPos;
+        int endPos;
+        int childCount;
+        int size;
+
+
+        @Override
+        public String toString() {
+            if (minValue==null || maxValue==null)
+                return null;
+
+            if(minValue.toString().equals(maxValue.toString()))
+                return minValue.toString();
+            return minValue.toString() + " to " + maxValue.toString();
+        }
+    }
 
 }
