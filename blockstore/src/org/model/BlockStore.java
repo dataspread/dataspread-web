24f9bed087f4bdd22f4788c9948a85c166b0040b
ET <xzhou14@illinois.edu>
2017-12-29 01:32:49 +0800

    Fix compile error

--- blockstore/src/org/model/BlockStore.java
+++ blockstore/src/org/model/BlockStore.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
