580bc08542313492eab9eb89e2863919d4431f96
Legan_dary <paul02110308@gmail.com>
2018-03-01 15:59:13 -0600

    Modify api for maven deploy

--- dsapi/src/org/ds/api/DataSpreadAPIServer.java
+++ dsapi/src/org/ds/api/DataSpreadAPIServer.java
@@ -3,15 +3,22 @@ package org.ds.api;
 import org.model.DBHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DataSpreadAPIServer {
+public class DataSpreadAPIServer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DataSpreadAPIServer.class);
+    }
 
 	public static void main(String[] args) {
         String url = "jdbc:postgresql://127.0.0.1:5432/ibd";
         String driver = "org.postgresql.Driver";
         String userName = "mangesh";
-        String password = "";
+        String password = "mangesh";
         DBHandler.connectToDB(url, driver, userName, password);
 		SpringApplication.run(DataSpreadAPIServer.class, args);
 	}
