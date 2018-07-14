1695f63903ee954c8e629710813556808689dc53
Mangesh Bendre <mangeshbendre@gmail.com>
2018-07-14 13:35:21 -0500

    Fixed pom files

--- dsapi/src/org/ds/api/DataSpreadAPIServer.java
+++ dsapi/src/org/ds/api/DataSpreadAPIServer.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DataSpreadAPIServer extends SpringBootServletInitializer {
@@ -23,4 +26,13 @@ public class DataSpreadAPIServer extends SpringBootServletInitializer {
 		SpringApplication.run(DataSpreadAPIServer.class, args);
 	}
 
+}
+
+@RestController
+class GreetingController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
