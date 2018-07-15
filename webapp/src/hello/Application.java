d45975452d88422a6dc7ba3dccd86a60cfe09b78
Mangesh Bendre <mangeshbendre@gmail.com>
2018-07-15 06:26:17 -0500

    typo

--- webapp/src/hello/Application.java
+++ webapp/src/hello/Application.java
@@ -2,14 +2,17 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer { {
+public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DataSpreadAPIServer.class);
+        return application.sources(Application.class);
     }
+
 
 
     public static void main(String[] args) {
