4831d10ca7366dc5518198b258cfa869d80c4316
Mangesh Bendre <mangeshbendre@gmail.com>
2018-08-12 20:58:42 -0500

    Remove serviceWorker.

--- dataspread-ui/src/index.js
+++ dataspread-ui/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Might enable this in future. Fix routing issue.
+//registerServiceWorker();
