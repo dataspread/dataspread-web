fc6fc700df56bdcfe8790579828fbdf2b87e1a6f
Mangesh Bendre <mangeshbendre@gmail.com>
2018-09-16 16:33:27 -0500

    To continue.

--- TestCode/src/GraphCompressor.java
+++ TestCode/src/GraphCompressor.java
@@ -1,13 +1,20 @@
 import org.zkoss.zss.model.CellRegion;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.*;
 import java.util.*;
+import java.util.List;
 
 
-public class GraphCompressor {
+public class GraphCompressor extends Frame {
+
+    private Collection<CellRegion> cellRegionCollection;
 
     private static class DependencyGraph {
 
@@ -41,12 +48,15 @@ public class GraphCompressor {
 
         public void printReverseGraph() {
             for (CellRegion dependsOn : reverseMap.keySet()) {
-                System.out.print(dependsOn.getReferenceString());
-                System.out.print("=>");
-                for (CellRegion depends : reverseMap.get(dependsOn))
-                    System.out.print(depends.getReferenceString() + " ");
-                System.out.print(getCoverageArea(reverseMap.get(dependsOn)));
-                System.out.println();
+                if (getCoverageArea(reverseMap.get(dependsOn)) > 20) {
+
+                    System.out.print(dependsOn.getReferenceString());
+                    System.out.print("=>");
+                    for (CellRegion depends : reverseMap.get(dependsOn))
+                        System.out.print(depends.getReferenceString() + " ");
+                    System.out.print(getCoverageArea(reverseMap.get(dependsOn)));
+                    System.out.println();
+                }
             }
         }
 
@@ -68,20 +78,40 @@ public class GraphCompressor {
             System.out.print("Start Area " + minArea);
 
             while (depends.size() > nodeLimit) {
-
-
+                greedyReduceOne(depends);
             }
         }
 
         public void greedyReduceOne(Set<CellRegion> depends) {
             List<CellRegion> cellRegionList = new ArrayList<>(depends);
             int best_i = 0, best_j = 1;
-            for (int i = 0; i < cellRegionList.size() - 1; i++)
-                for (int j = i + 1; i < cellRegionList.size(); j++) {
-
+            int best_area = getCoverageArea(depends);
+            for (int i = 0; i < cellRegionList.size() - 1; i++) {
+                for (int j = i + 1; j < cellRegionList.size(); j++) {
+                    CellRegion region1 = cellRegionList.get(i);
+                    CellRegion region2 = cellRegionList.get(j);
+                    depends.remove(region1);
+                    depends.remove(region2);
+                    CellRegion mergedBox = region1.getBoundingBox(region2);
+                    depends.add(mergedBox);
+                    int newArea = getCoverageArea(depends);
+                    if (newArea < best_area) {
+                        best_i = i;
+                        best_j = j;
+                        best_area = newArea;
+                    }
+                    depends.remove(mergedBox);
+                    depends.add(region1);
+                    depends.add(region2);
                 }
-
-
+            }
+            // Perform the best merge.
+            CellRegion region1 = cellRegionList.get(best_i);
+            CellRegion region2 = cellRegionList.get(best_j);
+            depends.remove(region1);
+            depends.remove(region2);
+            CellRegion mergedBox = region1.getBoundingBox(region2);
+            depends.add(mergedBox);
         }
 
         public void greedyCompressor() {
@@ -89,6 +119,11 @@ public class GraphCompressor {
         }
 
 
+    }
+
+    GraphCompressor() {
+        super("Graph Compressor demo");
+        prepareGUI();
     }
 
     private static DependencyGraph getGraphFile() throws IOException {
@@ -113,10 +148,81 @@ public class GraphCompressor {
         return dependencyGraph;
     }
 
+    private static DependencyGraph getGraphDB() throws SQLException, ClassNotFoundException {
+        String url = "jdbc:postgresql://localhost/dataset";
+        String user = "mangesh";
+        String password = "";
+        String driver = "org.postgresql.Driver";
+        Class.forName(driver);
+        Connection con = DriverManager.getConnection(
+                url, user, password);
 
-    public static void main(String args[]) throws IOException {
-        DependencyGraph dependencyGraph = getGraphFile();
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM survey_sheetdata " +
+                " WHERE filename = '3.17.17_1985-2015_industrials_pull.v7.xlsx'" +
+                " AND sheetname = 'Cash Flow Statement'");
+
+        DependencyGraph dependencyGraph;
+        dependencyGraph = new DependencyGraph();
+
+        while (rs.next()) {
+            String formula = rs.getString("formula");
+            if (formula != null) {
+                String tokens[] = formula.split("[ \t*+-/()<>!,]");
+                for (String token : tokens)
+                    if (token.matches("[A-Z]+[0-9]+:[A-Z]+[0-9]+") || token.matches("[A-Z]+[0-9]+"))
+                        dependencyGraph.putEdge(new CellRegion(rs.getInt("row")
+                                        , rs.getInt("col")),
+                                new CellRegion(token));
+            }
+        }
+        return dependencyGraph;
+    }
+
+    private void prepareGUI() {
+        setSize(400, 400);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        setSize(480, 200);
+        int o = 100;
+        int m = 10;
+        // g2.drawRect(50,50,100,100);
+        for (CellRegion cellRegion : cellRegionCollection) {
+            g2.drawRect(cellRegion.getRow() * m + o, cellRegion.getColumn() * m + o, cellRegion.getHeight() * m, cellRegion.getLength() * m);
+        }
+
+    }
+
+    public void setGraphToPlot(Collection<CellRegion> cellRegionCollection) {
+        this.cellRegionCollection = cellRegionCollection;
+    }
+
+    public static void main(String args[]) throws Exception {
+        GraphCompressor graphCompressor1 = new GraphCompressor();
+
+
+        //DependencyGraph dependencyGraph = getGraphFile();
+        DependencyGraph dependencyGraph = getGraphDB();
+
+
         dependencyGraph.printReverseGraph();
-        dependencyGraph.greedyCompressNode(new CellRegion("A1"), 2);
+
+        graphCompressor1.setGraphToPlot(dependencyGraph.reverseMap.get(new CellRegion("A1")));
+
+        dependencyGraph.greedyCompressNode(new CellRegion("A1"), 3);
+        System.out.println("Compressed Graph");
+        dependencyGraph.printReverseGraph();
+
+
+        graphCompressor1.setVisible(true);
+
     }
 }
