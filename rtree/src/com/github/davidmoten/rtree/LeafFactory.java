3d8639bfb8c199868d25bb78f5c041f03607fd1b
Mangesh Bendre <mangeshbendre@gmail.com>
2019-01-25 15:06:35 -0600

    Fix Performance test code

--- rtree/src/com/github/davidmoten/rtree/LeafFactory.java
+++
@@ -1,9 +0,0 @@
-package com.github.davidmoten.rtree;
-
-import java.util.List;
-
-import com.github.davidmoten.rtree.geometry.Geometry;
-
-public interface LeafFactory<T, S extends Geometry> {
-    Leaf<T, S> createLeaf(List<Entry<T, S>> entries, Context<T, S> context);
-}
--- rtree/src/com/github/davidmoten/rtree/geometry/HasGeometry.java
+++
@@ -1,6 +0,0 @@
-package com.github.davidmoten.rtree.geometry;
-
-public interface HasGeometry {
-
-    Geometry geometry();
-}
