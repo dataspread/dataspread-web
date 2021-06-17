47835ce38fcc759c3fed9e74a7bbefe44f43853f
Jonathan <jonathanyun@berkeley.edu>
2021-06-17 14:47:51 -0700

    added config file logic, depedencyTable options, and test cases

--- testcode/src/FormulaCompressionTest/CompressionTestMain.java
+++ testcode/src/FormulaCompressionTest/CompressionTestMain.java
@@ -6,18 +6,30 @@ import FormulaCompressionTest.runners.SyncTestRunner;
 import FormulaCompressionTest.utils.Util;
 import FormulaCompressionTest.tests.*;
 
-import org.zkoss.zss.model.impl.sys.DependencyTablePGImpl;
+import org.zkoss.zss.model.impl.sys.*;
 import org.zkoss.zss.model.sys.EngineFactory;
 import org.zkoss.zss.model.impl.SheetImpl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class CompressionTestMain {
 
     /******************************************************************************************************************
      * CONFIG ZONE ******************************************************************************************************
      ******************************************************************************************************************/
+
+    /**
+     * Config File Path
+     */
+    public static final String configPath = "../config.properties";
 
     /**
      * DB Connection Configuration
@@ -44,6 +56,7 @@ public class CompressionTestMain {
      * */
     public static String      spreadsheetString = "RunningTotalSlow";
     public static String      spreadsheetOperation = "LongestChainUpdate";
+    public static String[]    testArgs = null;
 
     /**
      * CONSTANTS, Not configured by users
@@ -71,12 +84,26 @@ public class CompressionTestMain {
 
     // TODO: add more dependencyTable options
     private static void configDependencyTable (String depTableString) {
-        if (depTableString.compareToIgnoreCase("PGImpl") == 0) {
-            EngineFactory.dependencyTableClazz = DependencyTablePGImpl.class;
-        } else {
-            System.out.println("Dependency table class " + depTableString
-                    + " is not supported yet");
-            System.exit(-1);
+        switch(depTableString.toLowerCase()) {
+            case "pgimpl":
+                EngineFactory.dependencyTableClazz = DependencyTablePGImpl.class;
+                break;
+            case "implv":
+                EngineFactory.dependencyTableClazz = DependencyTableImpl.class;
+                break;
+            case "implv2":
+                EngineFactory.dependencyTableClazz = DependencyTableImplV2.class;
+                break;
+            case "implv4":
+                EngineFactory.dependencyTableClazz = DependencyTableImplV4.class;
+                break;
+            case "comp":
+                EngineFactory.dependencyTableClazz = DependencyTableComp.class;
+                break;
+            default:
+                System.out.println("Dependency table class " + depTableString
+                        + " is not supported yet");
+                System.exit(-1);
         }
     }
 
@@ -85,21 +112,65 @@ public class CompressionTestMain {
                                     String sheetOperation,
                                     int depTblCacheSize,
                                     int compConstant) {
-        if (sheetString.compareToIgnoreCase("RunningTotalSlow") == 0) {
-            oneTest = new TestRunningTotalSlow(3000);
-        } else {
-            System.out.println("Spreadsheet " + sheetString
-                    + " is not supported yet");
-            System.exit(-1);
+        switch (sheetString.toLowerCase()) {
+            case "runningtotalslow":
+                oneTest = new TestRunningTotalSlow(Integer.parseInt(testArgs[0]));
+                break;
+            case "runningtotalfast":
+                oneTest = new TestRunningTotalFast(Integer.parseInt(testArgs[0]));
+                break;
+            case "rate":
+                oneTest = new TestRate(Integer.parseInt(testArgs[0]));
+                break;
+            case "random":
+                oneTest = new TestRandom(Integer.parseInt(testArgs[0]));
+                break;
+            case "customsheet":
+                oneTest = new TestCustomSheet(Paths.get(testArgs[0]));
+                break;
+            case "expschedule":
+                oneTest = new TestExpSchedule(Integer.parseInt(testArgs[0]), Integer.parseInt(testArgs[1]));
+                break;
+            default:
+                System.out.println("Spreadsheet " + sheetString
+                        + " is not supported yet");
+                System.exit(-1);
         }
 
         oneTest.configDepTable(depTblCacheSize, compConstant);
         oneTest.setTestOperation(sheetOperation);
     }
 
+    private static void readConfig() {
+        try (InputStream input = new FileInputStream(configPath)) {
+            Properties config = new Properties();
+            config.load(input);
+
+            url = config.getProperty("url");
+            dbDriver = config.getProperty("dbDriver");
+            userName = config.getProperty("username");
+            password = config.getProperty("password");
+
+            outFolder = config.getProperty("outFolder");
+
+            useSyncRunner = config.getProperty("useSyncRunner").toLowerCase().equals("true");
+            depTableClassString = config.getProperty("depTableClassString");
+            depTableCacheSize = Integer.parseInt(config.getProperty("depTableCacheSize"));
+
+            spreadsheetString = config.getProperty("spreadsheetString");
+            spreadsheetOperation = config.getProperty("spreadsheetOperation");
+            testArgs = new String[] {config.getProperty("testarg1"), config.getProperty("testarg2")};
+        } catch (Exception e) {
+            System.out.println("Error trying to open config file at " +
+                    new File(configPath).getAbsolutePath());
+            e.printStackTrace();
+        }
+    }
+
     public static void main (String[] args) {
 
         // Setup
+        readConfig();
         CompressionTestMain.basicSetup(outFolder);
         CompressionTestMain.configTestRunner(useSyncRunner, outFolder);
         CompressionTestMain.configDependencyTable(depTableClassString);
